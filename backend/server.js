8cbd5ba5cb5cfb3415cfd692f8034db97d738afd
Ana Paula Martins <anapaula.martins17@hotmail.com>
2019-06-30 16:05:34 -0300

    MERN - Iniciando o projeto

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose'); 
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json()); 
+
+mongoose.connect(`mongodb+srv://anapaula:mean123@cluster0-a0ykm.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser:true, useCreateIndex: true });  
+const connection = mongoose.connection; 
+ 
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfuly")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+}) 
