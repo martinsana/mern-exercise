f7dfefc7dbbbf8a7842f6dbed6a40aeee3512fc0
Ana Paula Martins <anapaula.martins17@hotmail.com>
2019-07-06 22:53:26 -0300

    Front-end development started

---
+++ src/components/create-user.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class CreateUsers extends Component {
+    render() {
+        return (
+        <div>
+            <p>You are on the Create Users component!</p>
+            
+        </div>
+        
+
+        )
+    }
+}
